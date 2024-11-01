08f3a781465a33793e910f517c542d341d0d91a6
flavien-hugs <flavienhugs@pm.me>
2024-11-01 20:23:38 +0000

    feat: update botoclient function

--- src/common/boto_client.py
+++ src/common/boto_client.py
@@ -1,76 +1,54 @@
-import re
+import logging
 from typing import Optional
 
 import boto3
 from botocore import exceptions
 from fastapi import Depends, status
-from urllib import parse
+
 from src.config import settings
 from .error_codes import SfsErrorCodes
 from .exception import CustomHTTPException
 
-_boto_client: Optional[boto3.client] = None
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+_log = logging.getLogger(__name__)
+
+_botoclient: Optional[boto3.client] = None
 
 
 def get_boto_client() -> boto3.client:
-    global _boto_client
+    global _botoclient
 
-    if _boto_client is None:
+    if _botoclient is None:
+        _log.info("==> Connecting to Minio server...")
+        _botoclient = boto3.client(
+            "s3",
+            endpoint_url=settings.STORAGE_HOST,
+            aws_access_key_id=settings.STORAGE_ACCESS_KEY,
+            aws_secret_access_key=settings.STORAGE_SECRET_KEY,
+            region_name=settings.STORAGE_REGION_NAME,
+        )
         try:
-            _boto_client = boto3.client(
-                "s3",
-                endpoint_url=settings.STORAGE_HOST,
-                aws_access_key_id=settings.STORAGE_ACCESS_KEY,
-                aws_secret_access_key=settings.STORAGE_SECRET_KEY,
-                region_name=settings.STORAGE_REGION_NAME,
-            )
-        except (exceptions.ClientError, exceptions.BotoCoreError) as err:
+            _botoclient.list_buckets()
+            _log.info("==> Connected to Minio server successfully !")
+        except (exceptions.ClientError, exceptions.BotoCoreError, exceptions.NoCredentialsError) as err:
+            _log.error("==> An error occurred while connecting to Minio server.")
             raise CustomHTTPException(
                 error_code=SfsErrorCodes.SFS_UNKNOWN_ERROR,
                 error_message=err.response.get("Error").get("Message"),
                 status_code=err.response.get("ResponseMetadata").get("HTTPStatusCode"),
             ) from err
 
-    return _boto_client
+    return _botoclient
 
 
-def replace_minio_url_base(domain: str, url: str) -> str:
-    """
-    Replace the Minio URL base with the provided domain
-
-    :param domain: The domain to replace the Minio URL base
-    :param url: The URL to replace the Minio URL base
-    """
-
-    new_url = parse.urlparse(url)
-    domain = new_url._replace(netloc=domain)
-    return domain.geturl()
-
-
-def is_valid_bucket_name(bucket_name: str) -> str:
-    """
-    Format a bucket name to lowercase and limits its length
-    """
-    formatted = bucket_name.lower()[:63]
-    if re.match(settings.APP_BUCKET_NAME_PATTERN, formatted):
-        raise CustomHTTPException(
-            error_code=SfsErrorCodes.SFS_INVALID_NAME,
-            error_message=f"Bucket name '{bucket_name}' is invalid. "
-            f"The bucket name must contain lowercase letters and numbers,"
-            f" and be between 3 and 63 characters long.",
-            status_code=status.HTTP_400_BAD_REQUEST,
-        )
-
-    return bucket_name
-
-
-def check_bucket_exist(bucket_name: str, boto_client: boto3.client = Depends(get_boto_client)) -> str:
+def check_bucket_exists(bucket_name: str, botoclient: boto3.client = Depends(get_boto_client)) -> str:
     """
     Check if bucket name exist
     """
 
     try:
-        boto_client.head_bucket(Bucket=bucket_name)
+        botoclient.head_bucket(Bucket=bucket_name)
+        _log.warning(f"==> Bucket '{bucket_name}' already exists.")
     except (exceptions.ClientError, exceptions.BotoCoreError) as exc:
         error_message = exc.response.get("Error", {}).get("Message", "An error occurred")
         status_code = int(exc.response.get("ResponseMetadata", {}).get("HTTPStatusCode", status.HTTP_400_BAD_REQUEST))
