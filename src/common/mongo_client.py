0dcbeabb8bfc9e214e201693fae38f3274cdc124
Flavien HUGS <flavienhugs@pm.me>
2024-11-02 16:47:36 +0000

    test: add tests to bucket endpoint, mongo client and boto (#6)
    * hotfix: update gitignore
    
    * hotfix: update docker-compose file - remove env-files config
    
    * hotfix: structured and enhancement project (#3)
    
    * hotfix: update readme project
    
    * feat: add new environment variable
    
    * feat: add package beanie-odm
    
    * fix: remove unsing config in docker-compose file
    
    * feat: update botoclient function
    
    * feat: cleanup error codes and add news
    
    * feat: remove unusing functions and update utils functionnal
    
    * feat: define bucket policy config
    
    * test: add tests for endpoints and functional (#5)

--- src/common/mongo_client.py
+++ src/common/mongo_client.py
@@ -1,8 +1,12 @@
+import logging
 from typing import Optional
 
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo.server_api import ServerApi
 
 _mongoclient: Optional[AsyncIOMotorClient] = None
+logging.basicConfig(format="%(message)s", level=logging.INFO)
+_log = logging.getLogger(__name__)
 
 
 async def config_mongodb_client(mongodb_uri: str) -> AsyncIOMotorClient:
@@ -15,5 +19,11 @@ async def config_mongodb_client(mongodb_uri: str) -> AsyncIOMotorClient:
     global _mongoclient
 
     if _mongoclient is None or _mongoclient.closed is True:
-        _mongoclient = AsyncIOMotorClient(mongodb_uri)
+        _mongoclient = AsyncIOMotorClient(mongodb_uri, server_api=ServerApi("1"))
+        try:
+            await _mongoclient.admin.command("ping")
+            _log.info("Pinged your deployment. You successfully connected to MongoDB")
+        except Exception as e:
+            _log.debug(f"Failed to connect to MongoDB: {e}")
+            _mongoclient = _mongoclient
     return _mongoclient
